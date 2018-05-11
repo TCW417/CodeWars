a5fa088d4fc60b984cc70a61d70dd8eb6be4575b
Tracy Williams <selpilot@gmail.com>
2018-05-10 19:28:11 -0700

    fixed non-regex solution

--- usernameRegex/solution.js
+++ usernameRegex/solution.js
@@ -9,8 +9,10 @@ function validateUsr(username) {
   let between = (c, min, max) => min <= c && c <= max;
   let validChar = (c) => between(c,'a','z') || between(c, '0','9') || c === '_';
   if (username.length < 4 || username.length > 16) return false;
-  username.forEach((c) => {
-    if (!validChar(c)) return false; 
+  for (let i of username) {
+    if (!validChar(i)) return false;
   }
   return true;
 }
+
+//BTW, I tried doing this using .forEach on username but codewars didn't like it.
